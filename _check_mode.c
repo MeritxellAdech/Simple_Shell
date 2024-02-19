655720148a1948faa11a7bdfde41da6a1b13defb
Merit <meritxelladech@gmail.com>
2024-02-19 17:28:31 +0000

    FEATURE: test whether the user is in the interactive mode or non-interactive

---
+++ _check_mode.c
@@ -0,0 +1,18 @@
+#include "simple_shell.h"
+
+/**
+ * check_mode - test whether the shell is in interactive mode
+ * or non-interactive mode
+ *
+ * Return: the mode I_MODE if the user can interact with the shell
+ * or NON_I_MODE if the user cannot
+ */
+int check_mode(void)
+{
+	int mode;
+
+	mode = isatty(STDIN_FILENO);
+	if (mode == 1)
+		return (I_MODE);
+	return (NON_I_MODE);
+}
