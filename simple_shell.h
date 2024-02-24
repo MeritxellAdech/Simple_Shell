4a75d6223df0a4e3d9a2b37373699338aae7bf19
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-02-24 08:03:31 +0000

    creted _accept.c which initiate the shell and return EOF when encountered and and header file with all the prototypes of our implementation

--- simple_shell.h
+++ simple_shell.h
@@ -3,6 +3,11 @@
 
 /*Headers*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 /*Macros*/
@@ -10,19 +15,18 @@
 #define NON_I_MODE 0
 #define ERR_OCCURED -1
 
+#define VALID_CMD 0
+#define INVALID_CMD 89
+#define IS_EXEC 1
+#define IS_NOT_EXEC 90
 
 /*Structs*/
 
-
-
 /*Function prototypes*/
 int _accept(char **buffer, size_t *bytes_read);
+int check_file(char *path);
+int _putchar(char c);
+int _exec(char *commands);
 int check_mode(void);
 
-
-
-
-
-
 #endif
-
